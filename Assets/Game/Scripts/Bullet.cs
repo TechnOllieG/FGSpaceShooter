42a1dd3a7915e664de9407da2817e784722210b9
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-08-21 15:27:55 +0200

    Added despawning of bullet

--- Assets/Game/Scripts/Bullet.cs
+++ Assets/Game/Scripts/Bullet.cs
@@ -4,7 +4,6 @@ namespace FG
 {
     public class Bullet : MonoBehaviour
     {
-
         public float damage;
 
         private Rigidbody2D rb;
@@ -20,6 +19,10 @@ namespace FG
             if (collider.CompareTag("Enemy"))
             {
                 collider.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+                Destroy(gameObject);
+            }
+            else if(collider.CompareTag("Wall"))
+            {
                 Destroy(gameObject);
             }
         }
--- Assets/Game/Scripts/ReloadScene.cs
+++ Assets/Game/Scripts/ReloadScene.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
-    [SerializeField]
     public void Reload()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
