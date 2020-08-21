fac85d19749544d55b4578ef5215d2f722b93492
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-08-21 18:37:57 +0200

    Added Main menu

---
+++ Assets/Game/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FG
+{
+    public class Menu : MonoBehaviour
+    {
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
