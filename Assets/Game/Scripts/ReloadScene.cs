f9adba30773ba22dd30b3dfe8f459645821dbc39
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-08-21 15:19:40 +0200

    Created ReloadScene script

---
+++ Assets/Game/Scripts/ReloadScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene
+{
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
