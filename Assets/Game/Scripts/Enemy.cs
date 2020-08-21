f959201dcf6d77cf81cc36c47e980b05bd138e5b
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-08-21 13:59:30 +0200

    Added bouncing enemies and hp to enemies

--- Assets/Game/Scripts/Enemy.cs
+++ Assets/Game/Scripts/Enemy.cs
@@ -4,19 +4,48 @@ namespace FG
 {
     public class Enemy : MonoBehaviour
     {
+        [Tooltip("The multiplier for which to multiply the enemies initial force added (as they enter the scene)")]
         public float enemySpeedMultiplier;
+        [Tooltip("The minimum velocity the enemy should bounce of the walls at")]
+        public float minVelocity;
+        [Tooltip("The hitpoints of the enemy, 1 will kill it in one machine gun shot")]
+        public int hp = 1;
 
+        private Rigidbody2D rb;
+        private Vector3 lastVelocity;
+        private float speed;
+        private Vector3 direction;
         private void Awake()
         {
-            GetComponent<Rigidbody2D>().AddForce(transform.up * enemySpeedMultiplier, ForceMode2D.Impulse);
+            rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * enemySpeedMultiplier, ForceMode2D.Impulse);
+        }
+        private void Update()
+        {
+            lastVelocity = rb.velocity;
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            GameObject.Destroy(this.gameObject);
             if(collision.gameObject.tag == "Player")
             {
+                GameObject.Destroy(this.gameObject);
                 collision.gameObject.GetComponent<PlayerController>().hp -= 1;
+            }
+            else if(collision.gameObject.tag == "Wall")
+            {
+                speed = lastVelocity.magnitude;
+                direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+                rb.velocity = direction * Mathf.Max(speed, minVelocity);
+            }
+            else if(collision.gameObject.tag == "Machine Gun")
+            {
+                hp--;
+                if(hp<=0)
+                {
+                    GameObject.Destroy(this.gameObject);
+                }
             }
         }
 
